52e975abeeb79354a659e0ed17a41f2a6357aaa5
daniinad <151610228@smkassalaambandung.sch.id>
2017-08-03 13:05:21 +0700

    Larapus

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,15 +38,20 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        &nbsp;
+                        <ul class="nav navbar-nav">
+                            @if (Auth::check())
+                            <li><a href="{{url('/home')}}">Dashboard</a></li>
+                            @endif
+                        </ul>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
+                            <li><a href="{{ url('/login') }}">Login</a></li>
+                            <li><a href="{{ url('/register') }}">Register</a></li>
+                            <li><a href="{{ url('/register') }}">Daftar</a></li>
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
